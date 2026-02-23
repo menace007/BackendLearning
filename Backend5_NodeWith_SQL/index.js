5be5e9c486a410dc7cc02f5d3f440b472f78e3c6
Manish Sharma <gautamanesh2469@gmail.com>
2026-02-23 23:52:04 +0530

    Feat: Complete CRUD operation project using MySQL and REST APIs

--- Backend5_NodeWith_SQL/index.js
+++ Backend5_NodeWith_SQL/index.js
@@ -4,8 +4,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-app.set("view engint", "ejs");
+const methodOverride = require("method-override");
+const {v4: uuidv4} = require("uuid");
+app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({extended: true}));
 // Create the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -66,6 +70,86 @@ app.get("/user", (req, res)=>{
 
     } catch(err){
         console.log(err);
+    }
+});
+
+// Add new user
+app.get("/user/new", (req, res)=>{
+    let id = uuidv4();
+    res.render("newUsers.ejs", {id});
+});
+
+app.post("/user/:id/new", (req,res)=>{
+    let{id} = req.params;
+    let{username, email, password} = req.body;
+    // console.log(username, email, password);
+    let q = `insert into user values ('${id}', '${username}', '${email}', '${password}')`;
+    try{
+        connection.query(q, (err, result)=>{
+            if(err) throw err;
+            // res.send(result);
+            res.render("/user");
+        })
+    }catch(err){
+        console.log(err);
+    }
+});
+//Edit route
+app.get("/user/:id/edit", (req, res)=>{
+    let {id} = req.params;
+    let q = `select * from user where id = '${id}'`;
+     try{
+        connection.query(q, (err, result) => {
+            if(err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", {user});
+        });
+
+    } catch(err){
+        console.log(err);
+        res.send("Some error in DB");
+    }
+});
+//Update route(DB)
+app.patch("/user/:id", (req, res)=>{
+    let {id} = req.params;
+    let {username: newUsername, password:formPassword} = req.body;
+    let q = `select * from user where id = '${id}'`;
+    try{
+        connection.query(q, (err,result)=>{
+            if(err) throw err;
+            let user = result[0];
+            if(formPassword != user.password){
+                res.send("Wrong password");
+            }
+            else{
+                let q1 = `update user set username = '${newUsername}' where id ='${id}'`;
+                try{
+                    connection.query(q1, (err, result)=>{
+                        if(err) throw err;
+                        res.redirect("/user");
+                    })
+                }catch(err){
+                    res.send(err);
+                }
+            }
+        });
+    } catch(err){
+        console.log(err);
+        res.send(err);
+    } 
+});
+
+app.delete("/user/:id", (req,res)=>{
+    let {id} = req.params;
+    let q = `delete from user where id = '${id}'`;
+    try{
+        connection.query(q, (err, result)=>{
+            if(err) throw err;
+            res.redirect("/user");
+        });
+    }catch(err){
+        res.send("Some error in DB");
     }
 });
 
