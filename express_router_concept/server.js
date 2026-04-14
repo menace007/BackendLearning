bd886858586b91d1d7338fa092385416cbc7b7e2
Manish Sharma <gautamanesh2469@gmail.com>
2026-04-14 23:36:07 +0530

    Add cookie concept and signed cookie concept

--- express_router_concept/server.js
+++ express_router_concept/server.js
@@ -2,8 +2,37 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser")
 
+app.use(cookieParser("secretcode"));
+
+// Cookies concept
+// app.get("/getcookies", (req, res)=>{
+//     res.cookie("greet", "hello");
+//     res.cookie("madein", "india");
+//     res.send("Sent you some cookies");
+// })
+
+// Signed Cookies
+app.get("/getsignedcookie", (req, res)=>{
+    res.cookie("made-in", "India", {signed: true});
+    res.send("Signed cookie sent!");
+})
+
+//Verify signed cookies
+app.get("/verify",(req, res)=>{
+    console.log(req.signedCookies);
+    res.send("Verified");
+})
+
+app.get("/greet", (req, res)=>{
+    let {name="anonynomous"} = req.cookies;
+    res.send(`Hi, ${name}`);
+})
+
+//Root route
 app.get("/", (req, res)=>{
+    console.dir(req.cookies);
     res.send("Hi, I am root");
 });
 
